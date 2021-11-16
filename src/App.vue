b79f534b0a3d6b3d92bc4b34746b17d9fd223fba
“Artyvas” <artemvasily@gmail.com>
2021-11-15 18:31:55 -0800

    Initial build of timer logic - beginning to add dynamic task list component - no styling so far

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,96 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
+    <!-- <div id="nav"> -->
+      <!-- <router-link to="/">Home</router-link>
       |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <router-view /> -->
+    <div id ="pomodoro">
+      <div id="pombuttons">
+        <button @click="setPomodoro()">Pomodoro</button>
+        <button @click="setShortBreak()">Short Break</button>
+        <button @click="setLongBreak()">Long Break</button>
+      </div>
+      <div id="timer">{{ totalTime }}</div>
+      <div>
+      <button v-if="paused" @click="startTimer()">Start</button>
+      <button v-if="!paused" @click="stopTimer">Stop</button>
+      </div>
+      <div id="taskList">
+        <ul>
+          <tasks
+          v-for="task in tasks"
+          :key="friends.id"
+          :id="task.id"
+          :name="task.name"
+          :estPom="task.estPom"
+          :isActive="task.isActive"
+          ></tasks>
+        </ul>
+      </div>
+    </div>
+
   </div>
 </template>
+
+<script>
+import Tasks from './components/Tasks.vue';
+export default {
+  components: { Tasks },
+  data() {
+    return {
+      tasks: [{
+        id: new Date().toISOString(),
+        name: 'Finish Vue App',
+        estPom: 1,
+        isActive: false
+        },
+        {
+        id: new Date().toISOString(),
+        name: 'Eat some tasty food',
+        estPom: 1,
+        isActive: false
+        },
+      
+      ],
+      timer: null,
+      resetTimer: false,
+      totalTime: (25 * 60),
+      paused: true,
+      playButton: false
+    }
+  },
+  methods: {
+    setPomodoro() {
+      this.totalTime = (25 * 60);
+    },
+    setShortBreak() {
+      this.totalTime = (5 * 60);
+    },
+    setLongBreak() {
+      this.totalTime = (15 * 60);
+    },
+    startTimer() {
+      this.paused = !this.paused
+      if(!this.paused) {
+        this.timer = setInterval(() => this.countdown(),1000);
+      } else {
+        clearInterval(this.timer);
+        this.playButton = true;
+      }
+    },
+    countdown() {
+      this.totalTime = this.totalTime - 1;
+    },
+    stopTimer() {
+      this.paused = true;
+      clearInterval(this.timer)
+    }
+  }
+}
+
+</script>
 
 <style>
 #app {
---
+++ src/components/Tasks.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>Insert check mark image</button>
+</template>
+
+<script>
+export default {
+  data() return {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
